6952a91d400ff0f88c8d0b83ac32134b723f1390
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-04-02 01:46:08 +0100

    Add convenience methods and scheduler shutdown on close
    & fix typo

--- solaris/bot/bot.py
+++ solaris/bot/bot.py
@@ -11,14 +11,14 @@ from solaris import Config
 
 
 class Bot(commands.Bot):
-    __slots__ = ("extensions", "scheduler")
+    __slots__ = ("_extensions", "scheduler")
 
     def __init__(self) -> None:
         self._extensions = [p.stem for p in Path(".").glob("./solaris/bot/extensions/*.py")]
+        logging.debug(f"Extensions found: {self._extensions}")
+
         self.scheduler = AsyncIOScheduler()
         self.scheduler.configure(timezone=utc)
-
-        logging.debug(f"Extensions found: {self._extensions}")
 
         super().__init__(
             command_prefix=Config.DEFAULT_PREFIX,
@@ -36,6 +36,7 @@ class Bot(commands.Bot):
         super().run(Config.TOKEN, reconnect=True)
 
     async def close(self) -> None:
+        self.scheduler.shutdown()
         await self.get_cog("Hub").on_shutdown()
         await super().close()
 
@@ -48,7 +49,7 @@ class Bot(commands.Bot):
     async def on_ready(self) -> None:
         self.scheduler.start()
         logging.info(f"{len(self.scheduler.get_jobs())} jobs scheduled.")
-        logging.info(f"Ready!")
+        logging.info("Ready!")
 
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot or isinstance(message.channel, discord.DMChannel):
@@ -64,3 +65,21 @@ class Bot(commands.Bot):
 
         logging.debug(f"Invoking command `{ctx.command.name}`")
         await self.invoke(ctx)
+
+    async def grab_user(self, arg):
+        try:
+            return self.get_user(arg) or await self.fetch_user(arg)
+        except (ValueError, discord.NotFound):
+            return None
+
+    async def grab_channel(self, arg):
+        try:
+            return self.get_channel(arg) or await self.fetch_channel(arg)
+        except (ValueError, discord.NotFound):
+            return None
+
+    async def grab_guild(self, arg):
+        try:
+            return self.get_guild(arg) or await self.fetch_guild(arg)
+        except (ValueError, discord.NotFound):
+            return None
